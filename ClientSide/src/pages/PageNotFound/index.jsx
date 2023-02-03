83898feafee2c939c4aa4e9938d8f6473b0c6f34
Phol <Pholibertlim@gmail.com>
2023-02-03 21:16:50 +0800

    fixed pagenotfound UI

--- ClientSide/src/pages/PageNotFound/index.jsx
+++ ClientSide/src/pages/PageNotFound/index.jsx
@@ -7,7 +7,7 @@ const PageNotFound = () => {
   return (
     <div className='bg-gray-500 w-screen h-screen flex flex-col justify-center items-center'>
       <h3 className='text-white text-2xl'>PageNotFound</h3>
-      <Link to='/'><button className='text-black p-1 flex justify-center items-center'><FontAwesomeIcon icon={faHome}/>Home</button></Link>
+      <Link to='/'><button className='bg-transparent text-black p-1 flex justify-center items-center border-none'><FontAwesomeIcon icon={faHome}/>Home</button></Link>
     </div>
   );
 };
--- ClientSide/src/routes/PrivateRoute.jsx
+++ ClientSide/src/routes/PrivateRoute.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Navigate } from 'react-router-dom';
 
 const PrivateRoute = ({children}) => {
-  const [state, setState] = useState(true);
+  const [state, setState] = useState(false);
   console.log(state)
   return state ? children : <Navigate to='/account/login'/>;
 };
