aa70d5da6157f02772fafc0467f2ea95ec3c62fd
Phol <Pholibertlim@gmail.com>
2023-02-04 10:45:51 +0800

    fixed with prettier

--- ServerSide/server.js
+++ ServerSide/server.js
@@ -1,37 +1,32 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import fetch from 'node-fetch';
-import cors from 'cors'
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.get('/Territories/All', async(req,res)=>{
+app.get('/Territories/All', async (req, res) => {
   const response = await fetch('https://netzwelt-devtest.azurewebsites.net/Territories/All');
   const data = await response.json();
-  res.status(200).json(data)
-})
+  res.status(200).json(data);
+});
 
-app.post('/Account/SignIn', async (req,res)=>{
+app.post('/Account/SignIn', async (req, res) => {
   const IncomingData = req.body;
 
   const response = await fetch('https://netzwelt-devtest.azurewebsites.net/Account/SignIn', {
     method: 'post',
     body: JSON.stringify(IncomingData),
-    headers: {'Content-Type': 'application/json'}
+    headers: { 'Content-Type': 'application/json' },
   });
   const data = await response.json();
-  res.status(200).json(data)
-})
-
-
-
-
-
+  res.status(200).json(data);
+});
 
 app.listen(port, () => {
   console.log(`app is listening to : ${port}`);
